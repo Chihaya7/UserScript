a86cb674d3bd670b6760384ae7f7c8bbe55828a5
Chihay7 <44675983+Chihaya7@users.noreply.github.com>
2026-09-14 08:35:01 +0800

    脚本添加action

--- wnacg/wnacg_auto_match_sync.py
+++ wnacg/wnacg_auto_match_sync.py
@@ -9,7 +9,9 @@ SOURCE_URL = "https://wn01.link/"
 # 需要更新的多个脚本文件
 SCRIPT_FILES = [
     "wnacg Layout Redesign Mobile.user.js",
-    "wnacg Reading history GIST backup.user.js"
+    "wnacg Reading history GIST backup.user.js",
+    "wnacg AutoPager.user.js",
+    "BodyDragResize.user.js" 
 ]
 
 def fetch_latest_urls():
